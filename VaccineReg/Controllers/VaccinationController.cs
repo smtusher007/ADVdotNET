fd14c8dd8bd565995858a196ea3f01b75388e7b9
tusher <smtusher007@gmail.com>
2022-10-01 21:29:43 +0600

    Vaccine registration 1.002

--- VaccineReg/Controllers/VaccinationController.cs
+++ VaccineReg/Controllers/VaccinationController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VaccineReg.Models;
 
 namespace VaccineReg.Controllers
 {
     public class VaccinationController : Controller
     {
         // GET: Vaccination
+
+        [HttpGet]
         public ActionResult Registration()
         {
             return View();
         }
+        
+        [HttpPost]
+        public ActionResult Confirm(Vaccine vaccine)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Confirm", "Vaccination");
+            }
+            ViewBag.VaccineRegistration = vaccine.name;
+            return View(vaccine);
+        }
+ 
+
     }
 }
