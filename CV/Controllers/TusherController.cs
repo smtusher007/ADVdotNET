049896918a139fabef09a62f48c148a13b1a6901
tusher <smtusher007@gmail.com>
2022-10-01 20:30:05 +0600

    CV 1.01

---
+++ CV/Controllers/TusherController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class TusherController : Controller
+    {
+        // GET: Tusher
+        public ActionResult HomePage()
+        {
+            return View("HomePage");
+        }
+        public ActionResult education()
+        {
+            return View();
+        }
+        public ActionResult projects()
+        {
+            return View();
+        }
+        public ActionResult references()
+        {
+            return View();
+        }
+    }
+}
